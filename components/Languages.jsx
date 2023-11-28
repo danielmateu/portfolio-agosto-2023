5e2826c19a2501321e0f8b7114f2814307622968
Daniel Mateu <danielmateu86@gmail.com>
2023-11-28 16:00:51 +0100

    spain and queen added

--- components/Languages.jsx
+++ components/Languages.jsx
@@ -1,12 +1,33 @@
 
 import Link from "next/link"
+import spain from '../public/assets/espana.png'
+import reina from '../public/assets/reina.png'
+import Image from "next/image"
 
 export const Languages = () => {
 
+
+
     return (
-        <div className="bg-red-500 z-[1000] sticky top-5 left-full mr-16 sm:mr-[85px] lg:mr-2 rounded-xl px-2 flex gap-2">
-            <Link href="/es" locale='es'>Es</Link>
-            <Link href="/en" locale='en'>En</Link> 
+        <div className="z-[1000] sticky top-6 left-full mr-16 sm:mr-[85px] lg:mr-2 rounded-xl px-2 flex items-center justify-center gap-2">
+            <Link href="/es" locale='es'>
+                <Image
+                    src={spain}
+                    alt="Cambiar a castellano"
+                    width={20}
+                    height={20}
+                    className="cursor-pointer hover:scale-110 transition-all"
+                />
+            </Link>
+            <Link href="/en" locale='en'>
+                <Image
+                    src={reina}
+                    alt="Change language to english"
+                    width={20}
+                    height={20}
+                    className="cursor-pointer hover:scale-110 transition-all"
+                />
+            </Link>
         </div>
     )
 }
